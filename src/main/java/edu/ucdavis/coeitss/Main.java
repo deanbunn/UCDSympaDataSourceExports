80bf5f2237937d1b0eedfcd170a8150001d8189e
deanbunn <deanbunn@gmail.com>
2025-07-23 15:23:28 -0700

    20250723_1522hrs Grad Majors

--- src/main/java/edu/ucdavis/coeitss/Main.java
+++ src/main/java/edu/ucdavis/coeitss/Main.java
@@ -1,5 +1,7 @@
 package edu.ucdavis.coeitss;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -10,11 +12,16 @@ public class Main {
         //Initiate IAM Worker
         UCDIAMWorker uiamWrkr = new UCDIAMWorker();
 
+        //Export Folder Location
+        String exportFolderLoc = "/home/dbunn/UCDTesting/";
+
         //Initiate Sympa Major Export Settings
         List<SympaMajorExport> lSympaMajorExports = new ArrayList<>();
         lSympaMajorExports.add(new SympaMajorExport("GECE", "CivilEnvironGradStudents.txt"));
-        //lSympaMajorExports.add(new SympaMajorExport("GBIM", "BiomedicalGradStudents.txt"));
-        //lSympaMajorExports.add(new SympaMajorExport("GCSI", "ComputerScienceGradStudents.txt"));
+        lSympaMajorExports.add(new SympaMajorExport("GBIM", "BiomedicalGradStudents.txt"));
+        lSympaMajorExports.add(new SympaMajorExport("GCSI", "ComputerScienceGradStudents.txt"));
+        lSympaMajorExports.add(new SympaMajorExport("GMAE", "MechanicalAeroGradStudents.txt"));
+        lSympaMajorExports.add(new SympaMajorExport("GEEC", "ElecticalGradStudents.txt"));
 
         try
         {
@@ -22,20 +29,39 @@ public class Main {
             for(SympaMajorExport sme : lSympaMajorExports)
             {
 
+                //Pull Unique List of Student Email Addresses Based Upon Major Code
                 HashSet<String> hstUsrEmails = uiamWrkr.Get_Campus_Addresses_for_Students_In_MajorCode(sme.MajorCode);
 
-                for(String usrEmail : hstUsrEmails)
+                //Initiate Writer for Export File
+                try(BufferedWriter bfWriter = new BufferedWriter(new FileWriter(exportFolderLoc + sme.ExportFileName)))
                 {
-                    System.out.println(usrEmail);
+
+                    //Write Out Each Returned Email Address to Export File
+                    for(String usrEmail : hstUsrEmails)
+                    {
+                        bfWriter.write(usrEmail);
+                        bfWriter.newLine();
+                    }
+
+                    //Close Out Writer
+                    bfWriter.close();
+
+                    //Display Progress
+                    System.out.println("Exported major " + sme.MajorCode + " to " + sme.ExportFileName);
+
                 }
-            }
+                catch(Exception eio)
+                {
+                    System.out.println(eio.toString());
+                }
+
+            }//End of lSympaMajorExports For
             
         }
         catch(Exception e)
         {
             System.out.println(e.toString());
         }
-        
-        
+
     }
 }
